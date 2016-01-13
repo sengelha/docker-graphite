87d8aadb00eec033c577e17bedbb79c3d93f8694
Steven Engelhardt <sengelha@gmail.com>
2016-01-12 22:28:14 -0600

    Point to dockerhub for docker images

--- runme.sh
+++ runme.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-GRAPHITE_CONTAINER_ID=551ae4a4853f
-CARBON_CONTAINER_ID=66cc8968f4e8
+GRAPHITE_DOCKER_TAG=sengelha/graphite:0.9.15.0
+CARBON_DOCKER_TAG=sengelha/carbon:0.9.15.0
+
+# Start carbon
+mkdir -p $PWD/volumes/carbon/opt/graphite/storage/whisper
+mkdir -p $PWD/volumes/carbon/var/log
+docker run -p 2003:2003 -v $PWD/volumes/carbon/opt/graphite/storage/whisper:/opt/graphite/storage/whisper -v $PWD/volumes/carbon/var/log:/var/log $CARBON_DOCKER_TAG
 
 # Start graphite-web
 mkdir -p $PWD/volumes/graphite/var/log
-docker run -p 80:80 -v $PWD/volumes/carbon/opt/graphite/storage/whisper:/opt/graphite/storage/whisper -v $PWD/volumes/graphite/var/log:/var/log $GRAPHITE_CONTAINER_ID
-
-# Start carbon
-#mkdir -p $PWD/volumes/carbon/opt/graphite/storage/whisper
-#mkdir -p $PWD/volumes/carbon/var/log
-#docker run -p 2003:2003 -v $PWD/volumes/carbon/opt/graphite/storage/whisper:/opt/graphite/storage/whisper -v $PWD/volumes/carbon/var/log:/var/log $CARBON_CONTAINER_ID
+docker run -p 80:80 -v $PWD/volumes/carbon/opt/graphite/storage/whisper:/opt/graphite/storage/whisper -v $PWD/volumes/graphite/var/log:/var/log $GRAPHITE_DOCKER_TAG
